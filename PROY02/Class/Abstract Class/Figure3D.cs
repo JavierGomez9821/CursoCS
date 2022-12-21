43b4975bf1d68d4be335eb1afcc95acb35dbcd26
JavierGomez9821 <jagomez@redsis.com>
2022-12-21 14:55:55 -0500

    21-12-2022 Inicio Clase

--- PROY02/Class/Abstract Class/Figure3D.cs
+++ PROY02/Class/Abstract Class/Figure3D.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace Geometry
 {
-    public class Figure3D
+    public abstract class Figure3D : Figure
     {
+        public float _volume;
+        public Figure3D(){}
+        public Figure3D(string name) : base(name) { }
+        public Figure3D(string name, string type, string lineColor, string fillColor) : base(name, type, lineColor, fillColor) { }
         
     }
 }
