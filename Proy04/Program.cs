10fdb1a91b05fca819e7954c0ff4e65a9e9d7e7d
JavierGomez9821 <120685168+JavierGomez9821@users.noreply.github.com>
2022-12-21 17:17:23 -0500

    21-12-2022 17:17

---
+++ Proy04/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int secretNumber = new Random().Next(1, 100);
+        ushort attempts = 0;
+        Console.Clear();
+        Console.WriteLine($"Adivine el numero secreto entre 1 y 100. En el menor numero de intentos: ");
+        Console.WriteLine($"Para Salir sin adivinar digite 0 ");
+        ushort userNumber = 1000;
+        Console.Write($"Ingrese el numero => ");
+        do
+        {
+            try
+            {
+                userNumber = ushort.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Debe Ingresar Numeros.");
+                // Console.WriteLine($"error {error} ");
+            }
+            
+            if (userNumber == 0) break;
+            attempts++;
+            if (userNumber > secretNumber)
+                Console.WriteLine($"Intento {attempts}. El numero ingresado es mayor que el numero secreto. ");
+            if (userNumber < secretNumber)
+                Console.WriteLine($"Intento {attempts}. El numero ingresado es menor que el numero secreto. ");
+
+        } while (userNumber != secretNumber);
+        if (userNumber == 0)
+            Console.WriteLine($"Ni en {attempts} intento(s) pudiste adivinar. El numero era {userNumber} ");
+        else
+            Console.WriteLine($"Felicitaciones! Completaste el juego en {attempts} intento(s). ");
+    }
+}
